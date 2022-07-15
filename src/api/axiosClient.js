3b1605a6c457f2612871bc96f40ff9a11b1c7cdc
murimichege <chegeeddie@gmail.com>
2022-07-15 16:43:47 +0300

    changed backend host for axios

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import queryString from 'query-string'
 
-const baseUrl = 'http://127.0.0.1:3000/api/'
+const baseUrl = 'https://vaccine-passport-backend.vercel.app:3000/api/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
